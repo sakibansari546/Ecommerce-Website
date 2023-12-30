934997327b57a150cc05072cfb051e84281a042b
School Practical Project <152527120+sakibansari546@users.noreply.github.com>
2023-12-30 09:28:01 +0530

    add product in JSON formate

---
+++ Hero/Main Files/product.js
@@ -0,0 +1,158 @@
+const product = [
+    {
+        "image": "/Hero/images/f1.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/f2.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/f3.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/f4.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/f5.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/f6.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/f7.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/f8.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/n1.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/n2.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/n3.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    },
+    {
+        "image": "/Hero/images/n4.jpg",
+        "brand": "adidas",
+        "productName": "Cartoon Astronaut T-Shirts",
+        "ratings": [
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>",
+            "<i class='fas fa-star'></i>"
+        ],
+        "price": "$34"
+    }
+]
