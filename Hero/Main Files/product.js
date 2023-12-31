9446d35b95e42fe0f2f38fb108a2ab45ac9e037c
School Practical Project <coclover9878@gmail.com>
2023-12-31 11:11:52 +0530

    hgjh

--- Hero/Main Files/product.js
+++ Hero/Main Files/product.js
@@ -1,18 +1,17 @@
-const product = [
+const products = [
     {
+        "id": 1,
         "image": "/Hero/images/f1.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
         "ratings": [
             "<i class='fas fa-star'></i>",
             "<i class='fas fa-star'></i>",
-            "<i class='fas fa-star'></i>",
-            "<i class='fas fa-star'></i>",
-            "<i class='fas fa-star'></i>"
         ],
         "price": "$34"
     },
     {
+        "id": 2,
         "image": "/Hero/images/f2.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -21,11 +20,11 @@ const product = [
             "<i class='fas fa-star'></i>",
             "<i class='fas fa-star'></i>",
             "<i class='fas fa-star'></i>",
-            "<i class='fas fa-star'></i>"
         ],
         "price": "$34"
     },
     {
+        "id": 3,
         "image": "/Hero/images/f3.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -33,12 +32,12 @@ const product = [
             "<i class='fas fa-star'></i>",
             "<i class='fas fa-star'></i>",
             "<i class='fas fa-star'></i>",
-            "<i class='fas fa-star'></i>",
             "<i class='fas fa-star'></i>"
         ],
         "price": "$34"
     },
     {
+        "id": 4,
         "image": "/Hero/images/f4.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -52,6 +51,7 @@ const product = [
         "price": "$34"
     },
     {
+        "id": 5,
         "image": "/Hero/images/f5.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -65,6 +65,7 @@ const product = [
         "price": "$34"
     },
     {
+        "id": 6,
         "image": "/Hero/images/f6.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -78,6 +79,7 @@ const product = [
         "price": "$34"
     },
     {
+        "id": 7,
         "image": "/Hero/images/f7.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -91,6 +93,7 @@ const product = [
         "price": "$34"
     },
     {
+        "id": 8,
         "image": "/Hero/images/f8.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -104,6 +107,7 @@ const product = [
         "price": "$34"
     },
     {
+        "id": 9,
         "image": "/Hero/images/n1.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -117,6 +121,7 @@ const product = [
         "price": "$34"
     },
     {
+        "id": 10,
         "image": "/Hero/images/n2.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -130,6 +135,7 @@ const product = [
         "price": "$34"
     },
     {
+        "id": 11,
         "image": "/Hero/images/n3.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -143,6 +149,7 @@ const product = [
         "price": "$34"
     },
     {
+        "id": 12,
         "image": "/Hero/images/n4.jpg",
         "brand": "adidas",
         "productName": "Cartoon Astronaut T-Shirts",
@@ -155,4 +162,4 @@ const product = [
         ],
         "price": "$34"
     }
-]
+];
--- Hero/Main Files/script.js
+++ Hero/Main Files/script.js
@@ -3,14 +3,66 @@ const closeBar = document.getElementById("close-bar");
 const nav = document.getElementById("navbar");
 
 if (bar) {
-    bar.addEventListener('click', ()=>{
+    bar.addEventListener('click', () => {
         nav.classList.add('active');
     })
 }
 
 if (closeBar) {
-    closeBar.addEventListener('click', ()=>{
+    closeBar.addEventListener('click', () => {
         nav.classList.remove('active');
         console.log("Clicked");
     })
+}
+
+
+
+
+
+
+onload();
+function onload() {
+    displayAllproductsInPage();
+    singleProductPage();
+}
+
+function displayAllproductsInPage() {
+    let productContainer = document.querySelector('.pro-container');
+    let innerHTML = '';
+    products.forEach((product) => {
+        let starHTML = product.ratings.map((rating) => rating).join('');
+
+        innerHTML += `
+        <div class="pro">
+                <img src="${product.image}" alt="">
+                <div class="des">
+                    <span>${product.brand}</span>
+                    <h5>${product.productName}</h5>
+                    <div class="star">
+                       ${starHTML}
+                    </div>
+                    <div class="price">
+                        <h4>${product.price}</h4>
+                        <a href="#">
+                            <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+                            <lord-icon src="https://cdn.lordicon.com/hyhnpiza.json" trigger="hover"
+                                colors="primary:#088178"
+                                style="width:35px;height:35px; color: #088178;background-color: #e8f6ea; border-radius: 50px; border: 1px solid #cce7d0;">
+                            </lord-icon>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `
+        return product.id;
+
+    });
+    productContainer.innerHTML = innerHTML;
+}
+
+
+function singleProductPage() {
+    let productContainer = document.querySelector('.pro-container');
+
+
 }
